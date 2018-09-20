a917165ace05f8ce12cf48646119dcac9ff53cd8
Godfred Akpan <31159357+godfredakpan@users.noreply.github.com>
2018-09-20 20:14:12 +0100

    Update healthissues.blade.php

--- resources/views/healthissues.blade.php
+++ resources/views/healthissues.blade.php
@@ -141,7 +141,7 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
-                            <h1>About Us</h1>
+                            <h1>Health Issues</h1>
                             <span></span>
                         </div>
                     </div>
